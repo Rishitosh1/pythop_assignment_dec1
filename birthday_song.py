2fa5fce8e3ab3a8cde53d94fddd99b52ee05db04
rishitosh <arishitosh@gmail.com>
2017-12-11 09:52:37 +0545

    print birthday song by asking name

---
+++ birthday_song.py
@@ -0,0 +1,6 @@
+def song(name):
+
+    print("{0} {1} {2} {3}\n{0} {1} {2} {3}\n{0} {1} {2} {3} {4} {5}\n{0} {1} {2} {3}" .format('happy' , 'birthday' , 'to' , 'you' , 'bruv' , name))
+
+name=input("enter a name for the song: ")
+song(name)
